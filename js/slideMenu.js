86651f21f12874ac43d15b24f71bd3a6883e40f6
hanfeelhoo <hanfeelhoo@naver.com>
2021-11-15 15:31:03 +0900

    git 실수 해결

--- js/slideMenu.js
+++ js/slideMenu.js
@@ -3,25 +3,15 @@ const colseMenuBtn = classList[1];
 
 const showMenuBtn = document.getElementsByClassName("head-menu-btn")[0];
 const menu = document.getElementById("slide-menu");
-let isClick = false;
 
 showMenuBtn.addEventListener("click", () => {
-    menu.style.display = "block";
     showMenuBtn.style.display = "none";
     console.log(colseMenuBtn);
     colseMenuBtn.style.display = "block";
 });
 
 colseMenuBtn.addEventListener("click", () => {
-  console.log("colse click");
   menu.style.display = "none";
   showMenuBtn.style.display = "block";
   colseMenuBtn.style.display = "none";
 });
-
-
-// function closeNav() {
-//   document.getElementById("mySidenav").style.width = "0";
-//   document.getElementById("main").style.marginLeft= "0";
-//   document.body.style.backgroundColor = "white";
-// }
