c1af4a460b33c34edf61fb4ddc68def771440fac
hanfeelhoo <hanfeelhoo@naver.com>
2021-11-15 15:37:56 +0900

    menu error 수정

--- js/slideMenu.js
+++ js/slideMenu.js
@@ -5,8 +5,8 @@ const showMenuBtn = document.getElementsByClassName("head-menu-btn")[0];
 const menu = document.getElementById("slide-menu");
 
 showMenuBtn.addEventListener("click", () => {
+    menu.style.display = "block";
     showMenuBtn.style.display = "none";
-    console.log(colseMenuBtn);
     colseMenuBtn.style.display = "block";
 });
 
