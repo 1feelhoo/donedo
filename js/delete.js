58f04500b35f7b6b8f361be8fbcb22d9280a1e49
hanfeelhoo <hanfeelhoo@naver.com>
2021-11-15 15:14:27 +0900

    delete.js 수정 완료

--- js/delete.js
+++ js/delete.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var cardList = document.querySelectorAll("#delete.delete-btn");
 
 console.log(cardList);
@@ -10,21 +9,3 @@ for (let i = 0; i < cardList.length; i++) {
     card.remove();
   });
 }
-=======
-var cardList;
-
-setInterval(() => {
-  cardList = document.querySelectorAll("#delete.delete-btn");
-  for (let i = 0; i < cardList.length; i++) {
-    cardList[i].addEventListener("click", () => {
-      const card =
-        cardList[i].parentElement.parentElement.parentElement.parentElement;
-      card.remove();
-    });
-  }
-}, 1000);
-
-
-
-
->>>>>>> 5759ca0 (삭제 방식 수정, 모달 반응형으로 적용, 메뉴창 완성)
